d8a972330ac277678f3aa343637cb2beacd76406
leejiseong <leejiseong@host.docker.internal>
2021-10-01 16:56:12 +0900

    예제4-2 시각

--- algorithm/src/baekjoon/Main.java
+++ algorithm/src/baekjoon/Main.java
@@ -3,8 +3,8 @@ package baekjoon;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int i = 5;
+		System.out.println(Integer.toString(i).contains("3"));
 	}
 
 }
